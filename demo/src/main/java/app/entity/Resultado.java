7edb0b51427b220cbb48478e43937f3ba98e2f6b
Isabela Migliorini <isabelamigli@gmail.com>
2024-08-01 14:00:56 -0300

    Atividade cálculos

---
+++ demo/src/main/java/app/entity/Resultado.java
@@ -0,0 +1,51 @@
+package app.entity;
+
+public class Resultado {
+	
+	private int soma;
+	private double media;
+	private int maior;
+	private int menor;
+	private int quantidade;
+
+	public int getSoma() {
+		return soma;
+	}
+
+	public void setSoma(int soma) {
+		this.soma = soma;
+	}
+
+	public double getMedia() {
+		return media;
+	}
+
+	public void setMedia(double media) {
+		this.media = media;
+	}
+
+	public int getMaior() {
+		return maior;
+	}
+
+	public void setMaior(int maior) {
+		this.maior = maior;
+	}
+	
+	public int getMenor() {
+		return menor;
+	}
+
+	public void setMenor(int menor) {
+		this.menor = menor;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+}
